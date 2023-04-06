d3226b4be1d6c860ac37f46852f5a7f05c41c534
FelipeCun <124890827+FelipeCun@users.noreply.github.com>
2023-04-05 21:54:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Meu comentario
+console.log("Olá Mundo");
+
+//prompt("Qual a sua idade ?")
+
+const diaDeNascimento = "15/04/1995";
+let texto = "Minha data de nascimento é: ";
+console.log(diaDeNascimento);
+console.log(texto);
+
+//Concatenando variaveis
+console.log(texto + diaDeNascimento);
+
+// Exemplos
+const primeiroNome = "Felipe";
+const segundoNome = "Cunha";
+let minhaIdade = "27";
+let estaEstudando = true;
+
+//imprimir
+console.log("Meu nome é "+ primeiroNome + " " + segundoNome + " " + "Tenho " + minhaIdade + ", e estou estudando " + estaEstudando);
