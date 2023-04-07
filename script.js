6a7692bdf2e121bbf218732cdc652d953b667715
FelipeCun <124890827+FelipeCun@users.noreply.github.com>
2023-04-06 21:58:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,78 @@
-//Meu comentario
-console.log("Olá Mundo");
+/*Exercicio 1
 
-//prompt("Qual a sua idade ?")
+let a = 10
+let b = 10
 
-const diaDeNascimento = "15/04/1995";
-let texto = "Minha data de nascimento é: ";
-console.log(diaDeNascimento);
-console.log(texto);
+console.log(b)
 
-//Concatenando variaveis
-console.log(texto + diaDeNascimento);
+b = 5
+console.log(a, b)
+*/
 
-// Exemplos
-const primeiroNome = "Felipe";
-const segundoNome = "Cunha";
-let minhaIdade = "27";
-let estaEstudando = true;
 
-//imprimir
-console.log("Meu nome é "+ primeiroNome + " " + segundoNome + " " + "Tenho " + minhaIdade + ", e estou estudando " + estaEstudando);
+/*Exercicio 2
+
+let c = 10
+let d = 20
+e = c
+d = e
+c = d
+
+console.log(c, d, e)
+*/
+
+
+/*Exercicio 3
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+*/
+
+
+/*
+//Exercicio de escrita de código
+
+//a) Declare uma variável para armazenar um nome, sem atribuir um valor.
+let nome = null;
+
+//b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
+let idade = null;
+
+//c) Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando typeof.
+alert(typeof idade);
+
+//d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+
+//g) Para finalizar, imprima na tela a mensagem: "Olá nome,  você tem idade anos". Onde nome e idade são os valores que o usuário inseriu
+alert("Olá " + nome + " você tem " + idade + " anos.");
+*/
+
+
+
+/*2. Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+    
+    a) Crie três novas variáveis, contendo as respostas
+    
+    b) Imprima na tela todas as perguntas seguidas por suas respectivas respostas. Por exemplo:
+
+    let resposta1 = prompt("o gremio vai sair campeão? (Sim/Não)")
+    let resposta2 = prompt("o inter vai sair campeão? (Sim/Não)")
+    let resposta3 = prompt("o caxias vai sair campeão? (Sim/Não)")
+
+    alert("o gremio vai sair campeão?" + resposta1 + "." + "\no inter vai sair campeão?" + resposta2 + "." + "\no caxias vai sair campeão?" + resposta3 + ".")*/
+
+
+    let a = 25;
+    let b = 10;
+    c = a
+    a = b
+    b = c
+
+    console.log("Novo valor de a é", a);
+    console.log("Novo valor de b é", b);
+
+
+    
