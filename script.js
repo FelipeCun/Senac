bb144782141dc851acff4953b4804f2bec5cd78c
FelipeCun <124890827+FelipeCun@users.noreply.github.com>
2023-04-10 21:53:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ alert("Olá " + nome + " você tem " + idade + " anos.");
 
     alert("o gremio vai sair campeão?" + resposta1 + "." + "\no inter vai sair campeão?" + resposta2 + "." + "\no caxias vai sair campeão?" + resposta3 + ".")*/
 
-
+    /*
     let a = 25;
     let b = 10;
     c = a
@@ -72,7 +72,104 @@ alert("Olá " + nome + " você tem " + idade + " anos.");
     b = c
 
     console.log("Novo valor de a é", a);
-    console.log("Novo valor de b é", b);
+    console.log("Novo valor de b é", b);*/
 
+    /*Faça um programa que receba dois números do usuário e faça as seguintes operações, imprimindo os resultados no console da seguinte forma:
+
+    
+    let num1 = parseFloat(prompt("Digite o primeiro número: "));
+    let num2 = parseFloat(prompt("Digite o segundo número: "));
+
+    soma = num1 + num2;
+    multiplicacao = num1 * num2;
+
+    console.log("O primeiro número somado ao segundo número resulta em: " + soma);
+    console.log("O primeiro número multiplicado pelo segundo número resulta em: " + multiplicacao);*/
+
+
+
+    /*let minhaIdade = 27;
+    alert("Minha idade atual é: " + minhaIdade);
+    
+    minhaIdade *= 32;
+    alert("Minha idade daqui a " + minhaIdade + " anos, será " + minhaIdade + " no pó da rabiola, petroleo, não tem jeito.");*/
+
+    /*
+    console.log(3+4);
+    console.log((3*5) /2);
+    console.log((4-5)* -1);
+    console.log(234%5);
+
+
+    let soma1 = 3;
+    let soma2 = 4;
+    console.log(resultado = soma1 + soma2);
+
+    let soma3 = 5;
+    console.log(resultado2 = (soma1 * soma3) /2);
+
+    let soma4 = 2;
+    console.log(resultado3 = resultado2 / 2);*/
+
+
+   /* Crie duas variáveis que guardem dois números. Imprima na tela as seguintes mensagens:
+
+    O primeiro número é diferente do segundo? True/False
+    O primeiro número é maior que o segundo? True/False
+    O primeiro número é menor que o segundo? True/False
+    O primeiro número é igual ao segundo? True/False
+
+
+    let num1 = 10;
+    let num2 = 5;
+
+    console.log(num1 !== num2);
+    console.log(num1 > num2);
+    console.log(num1 < num2);
+    console.log(num1 === num2);*/
+
+
+    /*Antes de começar, crie 3 variáveis: a, b e c. Atribua os valores true, false e true, respectivamente
+    a = true      b = false     c = true
+
+
+    Realize a operação: b && c
+    Realize a operação: a && c
+    Realize a operação: a && b && c
+    Realize a operação: a && b
+    
+    let a = true
+    let b = false
+    let c = true
+
+    console.log(b && c);
+    console.log(a && c);
+    console.log(a && b && c);
+    console.log(a && b);*/
+
+    /*Antes de começar, crie 3 variáveis: a, b e c. Atribua os valores true, false e true, respectivamente
+    a= true    b = false    c = true
+
+
+    Realize a operação: b || c
+    Realize a operação: a || c
+    Realize a operação: a || b || c
+    Realize a operação: a || b
+
+    let a = true
+    let b = false
+    let c = true
+
+    console.log(b || c);
+    console.log(a || c);
+    console.log(a || b || c);
+    console.log(a || b);*/
+
+    
+    /*Faça um programa que receba o nome, ano de nascimento de uma pessoa e o ano atual e mostre:	
+    O nome da pessoa
+    A idade dessa pessoa
+    Um true ou false que diz se ela é maior de idade
+    Quantos anos ela terá em 2050*/
 
     
